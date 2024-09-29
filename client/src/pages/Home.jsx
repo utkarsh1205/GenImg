7ec5ffe793eab919d0a88be207e37ffd00bdbc24
utkarsh1205 <utkarsh1205.agarwal@gmail.com>
2024-09-29 05:47:28 +0530

    Replace Dall-E with stable diffusion

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -67,7 +67,7 @@ const Home = () => {
     <section className="max-w-7xl mx-auto">
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">The Community Showcase</h1>
-        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">Browse through a collection of imaginative and visually stunning images by DALL-E AI</p>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">Browse through a collection of imaginative and visually stunning images by Stable Diffusion</p>
       </div>
 
       <div className="mt-16">
